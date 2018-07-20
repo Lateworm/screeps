a80f42ce5c4cb8996da8784beef5221262c56f97
Travis Remperl <travis.rempel@wealthbar.com>
2018-07-19 21:55:39 -0700

    Spawn builders, import from new file structure

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const roleHarvester = require('role.harvester');
-const roleUpgrader = require('role.upgrader');
-const roleBuilder = require('role.builder');
+const settings = require('settings');
+
+const roleWorkDelegate = require('role.work.delegate');
+const roleHarvester = require('role.work.task.harvest');
+const roleUpgrader = require('role.work.task.upgrade');
+const roleBuilder = require('role.work.task.build');
 
 module.exports.loop = function () {
     
@@ -17,45 +20,61 @@ module.exports.loop = function () {
             roleBuilder.run(creep);
         }
     }
-
-    // Calculate the cost of spawning a creep
-    const creepCost = bodyParts => {
-        return _.reduce(bodyParts, (cost, bodyPart) => cost + BODYPART_COST[bodyPart], 0);
-    }
     
     const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     const upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-
-    const harvesterSpawnTarget = 2
-    const upgraderSpawnTarget = 50
+    const builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 
     const roleCall = () => {
-        let harvesterCount = harvesters.length
-        let upgraderCount = upgraders.length
-        console.log(harvesterCount + ' harvesters, ' + upgraderCount + ' upgraders.')
+        console.log(harvesters.length + ' harvesters, '
+            + upgraders.length + ' upgraders, '
+            + builders.length + ' builders')
     }
 
-    if(Game.time % 10 === 0) {
+    if(Game.time % 20 === 0) {
         roleCall();
+    }
+
+    // Calculate the cost of spawning a creep
+    const newCreepCost = bodyParts => {
+        return _.reduce(bodyParts, (cost, bodyPart) => cost + BODYPART_COST[bodyPart], 0);
     }
     
     // Auto-spawn harvesters
-    // This creates a bunch of console spam at game start
-    // Add a check for affordability?
-    if(harvesters.length < harvesterSpawnTarget && Game.spawns.Spawn1.energy >= 200) {
-        const newName = 'Harvester' + Game.time;
-        console.log('Spawning ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
-            {memory: {role: 'harvester'}});
+    let newHarvesterBodyParts = [WORK, CARRY, MOVE]
+    let newHarvesterCost = newCreepCost(newHarvesterBodyParts)
+    if(harvesters.length < settings.harvesterSpawnTarget && Game.spawns.Spawn1.energy >= newHarvesterCost) {
+        const newHarvesterName = 'Harvester' + Game.time;
+        console.log('Spending ' + newHarvesterCost + ' energy to spawn ' + newHarvesterName);
+        console.log('#worthit!')
+        Game.spawns['Spawn1'].spawnCreep(newHarvesterBodyParts, newHarvesterName, {memory: {role: 'harvester'}});
     }
     
     // Auto-spawn upgraders AFTER spawning harvesters
-    if(harvesters.length >= harvesterSpawnTarget && upgraders.length < upgraderSpawnTarget && Game.spawns.Spawn1.energy >= 200) {
-        const newName = 'Upgrader' + Game.time;
-        console.log('Spawning ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+    let newUpgraderBodyParts = [WORK, CARRY, MOVE]
+    let newUpgraderCost = newCreepCost(newUpgraderBodyParts)
+    if(harvesters.length >= settings.harvesterSpawnTarget
+        && upgraders.length < settings.upgraderSpawnTarget
+        && Game.spawns.Spawn1.energy >= newUpgraderCost) {
+        const newUpgraderName = 'Upgrader' + Game.time;
+        console.log('Spending ' + newUpgraderCost + ' energy to spawn ' + newUpgraderName);
+        Game.spawns['Spawn1'].spawnCreep(newUpgraderBodyParts, newUpgraderName,
             {memory: {role: 'upgrader'}});
     }
+
+    // Auto-spawn builders AFTER spawning harveys and upgraders
+    let newBuilderBodyParts = [WORK, CARRY, MOVE]
+    let newBuilderCost = newCreepCost(newBuilderBodyParts)
+    if(harvesters.length >= settings.harvesterSpawnTarget
+        && upgraders.length >= settings.upgraderSpawnTarget
+        && builders.length < settings.builderSpawnTarget
+        && Game.spawns.Spawn1.energy >= newBuilderCost) {
+        const newBuilderName = 'Builder' + Game.time;
+        console.log('Spending ' + newBuilderCost + ' energy to spawn ' + newBuilderName);
+        Game.spawns['Spawn1'].spawnCreep(newBuilderBodyParts, newBuilderName,
+            {memory: {role: 'builder'}});
+    }
+
     
     // Print a spawning message when spawning
     if(Game.spawns['Spawn1'].spawning) {
