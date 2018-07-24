d7f3898a55ea3ae67b3f60c9843f1a362d89e57d
Travis Remperl <travis.rempel@wealthbar.com>
2018-07-24 00:00:04 -0700

    Use up to 2 sources

--- task.harvest.js
+++ task.harvest.js
@@ -1,10 +1,16 @@
 const taskHarvest = (creep) => {
 
 	const sources = creep.room.find(FIND_SOURCES);
+	const modIndex = creep.name.slice(-1)%2
+	if (modIndex > sources.length-1) {
+		modIndex = 0
+	}
+
+	// console.log(JSON.stringify(creep));
 	// const randomSourceIndex = Math.random(0, sources.length-1)
-	if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-		creep.moveTo(sources[1]); // defualt was 0, but that one was farther away
-		// TODO: move to closest source
+
+	if(creep.harvest(sources[modIndex]) == ERR_NOT_IN_RANGE) {
+		creep.moveTo(sources[modIndex]); // TODO: move to closest source
 	}
 
 };
