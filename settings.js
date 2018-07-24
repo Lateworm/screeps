b1b726a454f9798301cf5f63656942700793c016
Travis Remperl <travis.rempel@wealthbar.com>
2018-07-23 22:13:30 -0700

    Fix whatever the hell that was....

--- settings.js
+++ settings.js
@@ -1,11 +1,7 @@
 const settings = {
 
   // Spawn settings
-<<<<<<< HEAD
-  workerSpawnTarget: 50,
-=======
   workerSpawnTarget: 15,
->>>>>>> Tweak things after connecting to server
 
   // Console settings
   roleCallOnTick: 30,
